7337a7c2367f0cb7458bd3bf2cced60a6197cb07
Jeremy O'Daniel <jeremy.odaniel@gmail.com>
2022-05-21 13:13:08 -0500

    Updated table names in schema and added quotes where missing in seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,13 +5,13 @@ VALUES
   ('Operations');
 
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES
-(Jr Dev, 70000, 1),
-(Sr Dev, 90000, 1),
-(Sales, 80000, 2),
-(Project Manager, 100000, 3),
-(CIO, 130000, 3);
+('Jr Dev', 70000, 1),
+('SrDev', 90000, 1),
+('Sales', 80000, 2),
+('Project Manager', 100000, 3),
+('CIO', 130000, 3);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
