70109444a24857f5cd2a1b176f00b1f31c9f077c
Jeremy O'Daniel <jeremy.odaniel@gmail.com>
2022-05-22 19:03:43 -0500

    Create connection.js and modularize the connection to the DB

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
+const connection = require('./connection');
 const inquirer = require('inquirer');
-const mysql = require('mysql2');
-
 require('console.table');
 
 
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    user: 'root',
-    password: 'JODan00)',
-    database: 'employees',
-  },
-  console.log('Connected to the employees database.')
-);
 
